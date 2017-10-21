bcc595c610c2b7cbf2aed72785c7467ff21cbf05
Federico Kauffman <federico@wyeworks.com>
2017-10-21 20:42:14 -0300

    Move writer and reader flow to functions

--- public/demos/heart-rate-monitor/app.js
+++ public/demos/heart-rate-monitor/app.js
@@ -3,6 +3,38 @@ import HeartRateSensor from './heart-rate-sensor.js';
 import DatabaseBuilder from './database-builder.js';
 import ReaderDatabase from './reader-database.js';
 
+async function setupReader(database, barChart) {
+  database.onDataReceived((rate) => {
+    barChart.addData(rate);
+  });
+}
+
+async function setupWriter(database,barChart, connectButton, disconnectButton, heartRateSensor) {
+  connectButton.hidden = false;
+  disconnectButton.hidden = false;
+
+  connectButton.addEventListener('click', async () => {
+    connectButton.value = 'Connecting...';
+    connectButton.disabled = true;
+
+    await heartRateSensor.connect();
+
+    connectButton.value = 'Connected';
+
+    heartRateSensor.onHeartBeat((rate) => {
+      database.push(rate);
+      barChart.addData(rate);
+    });
+  })
+
+  disconnectButton.addEventListener('click', async () => {
+    connectButton.value = 'Connect';
+    connectButton.disabled = false;
+
+    await heartRateSensor.disconnect();
+  })
+}
+
 async function start() {
   const heartRateCanvas = document.getElementById('heart-rate');
   const connectButton = document.getElementById('connect-button');
@@ -15,33 +47,15 @@ async function start() {
   const database = await DatabaseBuilder.getDatabase();
 
   if (database instanceof ReaderDatabase) {
-      database.onDataReceived((rate) => {
-        barChart.addData(rate);
-      });
+      setupReader(database, barChart);
     } else {
-      connectButton.hidden = false;
-      disconnectButton.hidden = false;
-
-      connectButton.addEventListener('click', async () => {
-        connectButton.value = 'Connecting...';
-        connectButton.disabled = true;
-
-        await heartRateSensor.connect();
-
-        connectButton.value = 'Connected';
-
-        heartRateSensor.onHeartBeat((rate) => {
-          database.push(rate);
-          barChart.addData(rate);
-        });
-      })
-
-      disconnectButton.addEventListener('click', async () => {
-        connectButton.value = 'Connect';
-        connectButton.disabled = false;
-
-        await heartRateSensor.disconnect();
-      })
+      setupWriter(
+        database,
+        barChart,
+        connectButton,
+        disconnectButton,
+        heartRateSensor
+      );
     }
 }
 
