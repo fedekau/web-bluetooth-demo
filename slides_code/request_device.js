c2dec775c26283443880f8b8c91ef01ce80b0ffd
Federico Kauffman <federico@wyeworks.com>
2017-11-07 21:42:49 -0300

    Change event

--- slides_code/request_device.js
+++ slides_code/request_device.js
@@ -11,10 +11,13 @@ button.addEventListener('click', function(event) {
 // Or we can filter by different criterias, but not both.
 // Some services have standard names, others don't.
 // You have to see the specification sheet of the manufacturer.
-button.addEventListener('pointerup', function(event) {
+button.addEventListener('click', function(event) {
+  const SERVICE_1_ID = 0x12345678;
+  const SERVICE_2_UUID = '99999999-0000-1000-8000-00805f9b34fb';
+
   navigator.bluetooth.requestDevice({
     filters: [{
-      services: ['heart_rate', 0x12345678, '99999999-0000-1000-8000-00805f9b34fb']
+      services: ['heart_rate', SERVICE_1_ID, SERVICE_2_UUID]
     }]
   })
 });
